b3049e79c5afd860dd702979c7a8888eb12534dc
Damon Polioudakis UCLA mbp <damon.polioudakis@gmail.com>
2016-04-06 17:30:09 -0700

    Add plots

--- Determine_Usable_Cells.R
+++ Determine_Usable_Cells.R
@@ -10,6 +10,9 @@ sessionInfo()
 
 library(ggplot2)
 
+outGraphs <- "../analysis/graphs/Determine_Usable_Cells_"
+graphCodeTitle <- "Determine_Usable_Cells.R"
+
 # Picard Sequencing Statistics
 picStatsDF <- read.csv("../metadata/PicardToolsQC.csv")
 
@@ -18,29 +21,43 @@ metDatDF <- read.table("../metadata/Compiled_Metadata_20160229.txt"
                        , header = TRUE, sep = "\t")
 
 exDatDF <- read.csv("../data/htseq/merged/Exprs_HTSCexon.csv", row.names = 1)
+################################################################################
 
-colnames(picStatsDF)
+## Format
 
 metDatDF <- metDatDF[order(as.factor(metDatDF$HTseq_IDs)), ]
 metDatDF$HTseq_IDs
 
 exDatDF <- exDatDF[ ,order(colnames(exDatDF))]
-colnames(exDatDF)
 
 picStatsDF <- picStatsDF[order(picStatsDF$X), ]
-picStatsDF$X
 
 picStatsDF <- picStatsDF[picStatsDF$X %in% metDatDF$HTseq_IDs, ]
 
 picStatsDF[! picStatsDF$X %in% metDatDF$HTseq_IDs, ]
 
-# pctUM <- picStatsDF$PF_READS_ALIGNED/picStatsDF$TOTAL_READS
-nMapGenes <- apply(head(exDatDF, -97), 2, sum)
+
+## Sum ERCCs for each sample
 nMapERCC <- apply(head(tail(exDatDF, 97), -5), 2, sum)
 
-pctERCC <- nMapERCC/picStatsDF$PF_READS_ALIGNED
-round(pctERCC, 2)
 
-plot(picStatsDF$PF_READS_ALIGNED/(10^6), pctERCC)
+## Plots
 
-plot(picStatsDF$PF_READS_ALIGNED/(10^6), (1 -metDatDF$Pct_Uniquely_Mapped))
+# Histogram: Percentage of uniquely mapped reads - Mouse
+pdf(paste0(outGraphs, "Bargraph_ERCC_Counts.pdf"))
+barplot(nMapERCC
+     , main = paste(graphCodeTitle
+                    , "Raw ERCC counts for each capture site"
+                    , sep = "\n")
+     , xlab = "Capture Sites"
+     , ylab = "Counts")
+dev.off()
+
+pdf(paste0(outGraphs, "Histogram_ERCC_Counts.pdf"))
+hist(nMapERCC, breaks = 0:130
+     , main = paste(graphCodeTitle
+                    , "Histogram: ERCC Counts Per Sample"
+                    , sep = "\n")
+     , xlab = "ERCC Counts Per Sample")
+dev.off()
+
